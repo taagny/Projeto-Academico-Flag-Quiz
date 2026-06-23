88c102c79a7a1c00a724ce6a6a204391c2124a83
taagny <taagnyaugusto3214@gmail.com>
2026-06-23 15:44:22 -0300

    feat: adiciona lista de bandeiras

---
+++ src/data/flags.js
@@ -0,0 +1,24 @@
+const FLAGS = [
+  { country: 'Brasil', code: 'br', uri: 'https://flagcdn.com/w320/br.png' },
+  { country: 'Estados Unidos', code: 'us', uri: 'https://flagcdn.com/w320/us.png' },
+  { country: 'Japão', code: 'jp', uri: 'https://flagcdn.com/w320/jp.png' },
+  { country: 'Alemanha', code: 'de', uri: 'https://flagcdn.com/w320/de.png' },
+  { country: 'França', code: 'fr', uri: 'https://flagcdn.com/w320/fr.png' },
+  { country: 'Itália', code: 'it', uri: 'https://flagcdn.com/w320/it.png' },
+  { country: 'Canadá', code: 'ca', uri: 'https://flagcdn.com/w320/ca.png' },
+  { country: 'Argentina', code: 'ar', uri: 'https://flagcdn.com/w320/ar.png' },
+  { country: 'Portugal', code: 'pt', uri: 'https://flagcdn.com/w320/pt.png' },
+  { country: 'Espanha', code: 'es', uri: 'https://flagcdn.com/w320/es.png' },
+  { country: 'Reino Unido', code: 'gb', uri: 'https://flagcdn.com/w320/gb.png' },
+  { country: 'China', code: 'cn', uri: 'https://flagcdn.com/w320/cn.png' },
+  { country: 'Rússia', code: 'ru', uri: 'https://flagcdn.com/w320/ru.png' },
+  { country: 'México', code: 'mx', uri: 'https://flagcdn.com/w320/mx.png' },
+  { country: 'Austrália', code: 'au', uri: 'https://flagcdn.com/w320/au.png' },
+  { country: 'Índia', code: 'in', uri: 'https://flagcdn.com/w320/in.png' },
+  { country: 'Coreia do Sul', code: 'kr', uri: 'https://flagcdn.com/w320/kr.png' },
+  { country: 'África do Sul', code: 'za', uri: 'https://flagcdn.com/w320/za.png' },
+  { country: 'Turquia', code: 'tr', uri: 'https://flagcdn.com/w320/tr.png' },
+  { country: 'Suécia', code: 'se', uri: 'https://flagcdn.com/w320/se.png' },
+];
+
+export default FLAGS;
